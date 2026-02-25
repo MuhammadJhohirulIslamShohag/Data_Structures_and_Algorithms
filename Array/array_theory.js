a731154f97b7c6b17eecb75b06aadc306af8c3a1
Muhammad Jhohirul Islam <91692259+MuhammadJhohirulIslamShohag@users.noreply.github.com>
2026-02-25 13:39:23 +0000

    Update array_theory.js
    [added] theory of Array

--- Array/array_theory.js
+++ Array/array_theory.js
@@ -3,5 +3,6 @@
     that means one after another in memory.
     2. If you want to reduce operation you need use space
     3. Array biggest problems is a it's fixed capacity.
+    4. Pointer is a location of the data. Through this pointer, you can make custom data structure. Through this first data structure is linked list.
      
 */
