5f6ac786374f98b26441ef506fe77b268a839e5d
Muhammad Jhohirul Islam <91692259+MuhammadJhohirulIslamShohag@users.noreply.github.com>
2026-02-25 14:22:36 +0000

    Update array_theory.js

--- Array/array_theory.js
+++ Array/array_theory.js
@@ -3,6 +3,7 @@
     that means one after another in memory.
     2. If you want to reduce operation you need use space
     3. Array biggest problems is a it's fixed capacity.
-    4. Pointer is a location of the data. Through this pointer, you can make custom data structure. Through this first data structure is linked list.
+    4. Pointer(memory address)is a location of the data. Through this pointer, you can make custom data structure. Through this first data structure is linked list. JavaScript pointer another name is reference but c, c++ is the pointer.
+
      
 */
