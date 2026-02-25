aa3ad5845976fe4898da6f5bef43384f32b22d04
Muhammad Jhohirul Islam <91692259+MuhammadJhohirulIslamShohag@users.noreply.github.com>
2026-02-25 13:27:47 +0000

    Update array_theory.js
    [added] Array related theory

--- Array/array_theory.js
+++ Array/array_theory.js
@@ -2,5 +2,6 @@
     1. Array is linear data structure, it sometime called list, organize items sequentially, 
     that means one after another in memory.
     2. If you want to reduce operation you need use space
+    3. Array biggest problems is a it's fixed capacity.
      
 */
