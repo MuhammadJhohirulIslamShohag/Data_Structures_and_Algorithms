7bac117ebfef31902af9fbee30167522f779182f
Muhammad Jhohirul Islam <91692259+MuhammadJhohirulIslamShohag@users.noreply.github.com>
2026-03-17 16:50:10 +0000

    Update pattern-one.js
    ( theory ) pattern for data structure and algorithm

---
+++ Pattern/pattern-one.js
@@ -0,0 +1,14 @@
+/*
+
+Pattern - > nested loops ( outer and inner )
+
+1. for outer loop, count the number of lines/rows, that will determine what ourouter loop is going to be 
+
+2. for the inner loop we have to focus on the columns, and connect them somehow to the rows 
+
+3. whatever you are printing just '*' / '1', inner for loop.
+
+4. observe symmetry - optional 
+
+
+*/
