c4bdf235d4cc7e7bd09b63bd35b9298bdcbc18a0
Muhammad Jhohirul Islam <91692259+MuhammadJhohirulIslamShohag@users.noreply.github.com>
2026-05-26 18:39:36 +0100

    Update pattern-one.js
    (Done) pattern One

--- Pattern/pattern-one.js
+++ Pattern/pattern-one.js
@@ -12,3 +12,15 @@ Pattern - > nested loops ( outer and inner )
 
 
 */
+
+const patternOne = (n) => {
+  for(let i = 0; i < n; i++){
+    let line = '';
+    for(let j = 0; j < n; j++){
+      line += '*' + ' '
+    }
+    console.log(line);
+  }
+  
+}
+patternOne(5)
