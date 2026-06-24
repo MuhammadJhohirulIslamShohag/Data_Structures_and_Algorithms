65cbe884397e4d6a7cd3ce5c8f4f545909b72f1f
Muhammad Jhohirul Islam <91692259+MuhammadJhohirulIslamShohag@users.noreply.github.com>
2026-06-24 12:40:26 +0100

    Create max_consecutive_ones.js
    (done) solved the problem

---
+++ Array/max_consecutive_ones.js
@@ -0,0 +1,23 @@
+/*
+ Count Maximum Consecutive One's in the array
+*/
+
+// Brute Force — O(n²)
+const maxConsecutiveOnesBrute = (arr) => {
+    let n = arr.length;
+    let maxCount = 0;
+    let count = 0;
+    
+    for(let i = 0; i < n; i++){
+        if(arr[i] == 1) {
+            count += 1;
+            maxCount = Math.max(maxCount, count);
+        } else {
+            count = 0;
+        }
+    }
+    return maxCount;
+}
+
+const arr = [1, 1, 0, 1, 1, 1, 0, 1];
+console.log(maxConsecutiveOnesBrute(arr)); // 3
