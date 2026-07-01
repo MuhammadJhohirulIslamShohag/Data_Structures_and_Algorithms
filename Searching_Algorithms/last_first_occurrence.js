80f23f88fe22719454dc0dd311d834d7202d9c60
Muhammad Jhohirul Islam <91692259+MuhammadJhohirulIslamShohag@users.noreply.github.com>
2026-07-01 12:33:02 +0100

    Update last_first_occurrence.js
    (done) solved problem with brute force way

--- Searching_Algorithms/last_first_occurrence.js
+++ Searching_Algorithms/last_first_occurrence.js
@@ -1,3 +1,36 @@
 /* 
   First and Last Occurrence in a Sorted Array 
 */
+
+// Brute Force 
+function lastOccurrenceBrute(arr, target) {
+    
+    let n = arr.length;
+    let lastIndex = -1;
+    
+    for(let i = n - 1; i >= 0; i--){
+        if(arr[i] == target){
+            lastIndex = i;
+            break;
+        }
+    }
+    return lastIndex;
+}
+
+console.log(lastOccurrenceBrute([3, 4, 13, 13, 13, 20, 40], 13))
+
+function firstOccurrenceBrute(arr, target) {
+    
+    let n = arr.length;
+    let firstIndex = -1;
+    
+    for(let i = 0; i < n; i++){
+        if(arr[i] == target){
+            firstIndex = i;
+            break;
+        }
+    }
+    return firstIndex;
+}
+
+console.log(firstOccurrenceBrute([3, 4, 13, 13, 13, 20, 40], 13))
