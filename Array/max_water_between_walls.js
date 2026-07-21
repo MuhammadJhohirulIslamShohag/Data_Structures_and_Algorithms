e93a2b47b350d134a9a9385a73b6df71a36e92ec
Muhammad Jhohirul Islam <91692259+MuhammadJhohirulIslamShohag@users.noreply.github.com>
2026-07-21 12:36:03 +0100

    Create max_water_between_walls.js
    (done) new file created.

---
+++ Array/max_water_between_walls.js
@@ -0,0 +1,7 @@
+/*
+   Maximum Water Trapped Between Walls
+Languages
+
+Given an array walls of wall heights, calculate the maximum volume of water that will be trapped between two walls and the x-axis after a heavy downpour.
+
+*/
