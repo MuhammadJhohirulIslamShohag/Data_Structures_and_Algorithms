9b3e3d00a5b4d31853703b1c9f8caf5682355a59
Muhammad Jhohirul Islam <91692259+MuhammadJhohirulIslamShohag@users.noreply.github.com>
2026-08-02 11:32:12 +0100

    Create binary_tree_equal.js
    (done) new file created.

---
+++ Binary_Search_Trees/binary_tree_equal.js
@@ -0,0 +1,9 @@
+/*
+  Binary Tree Equal
+
+Given the root nodes of two binary trees, a and b, determine whether the two trees are equal.
+
+Two binary trees are considered equal if they have the same structure and the corresponding nodes in both trees have the same values.
+
+*/
+
