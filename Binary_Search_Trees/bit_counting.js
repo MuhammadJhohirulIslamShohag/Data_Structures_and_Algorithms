0cbe8fcaf4c535d9b3ae7238137d024e91bd35c9
Muhammad Jhohirul Islam <91692259+MuhammadJhohirulIslamShohag@users.noreply.github.com>
2026-09-15 20:46:21 +0100

    Create bit_counting.js
    (done) added new problem

---
+++ Binary_Search_Trees/bit_counting.js
@@ -0,0 +1,8 @@
+/*
+
+****** Bit Counting ******
+
+Given a non-negative integer n, write a function that takes the non-negative integer n and return an array result of size n + 1 where result[i] represents the number of active bits (set bits to 1) in the binary representation of the integer i (where 0 <= i <= n).
+
+*/
+
